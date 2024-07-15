fafc543ecae745401d1852b732120328e18573ad
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-07-15 20:17:53 +0700

    Update 2042

---
+++ crud-products/src/app/components/AllProducts/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from '@/Sidebar/Sidebar'
+import React from 'react'
+
+type Props = {}
+
+export default function AllProducts({}: Props) {
+  return (
+    <div>
+        <Sidebar/>
+        <h1>AllProducts</h1>
+    </div>
+  )
+}
--- crud-products/src/app/components/Login/Login.tsx
+++ crud-products/src/app/components/Login/Login.tsx
@@ -29,7 +29,7 @@ function Login({}: Props) {
                   type="email"
                   required
                   autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -52,7 +52,7 @@ function Login({}: Props) {
                   type="password"
                   required
                   autoComplete="current-password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
